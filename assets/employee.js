ce678ddcb594f615f1bb93214d052759470378d9
JonkHunkle <88416486+JonkHunkle@users.noreply.github.com>
2021-11-18 12:18:43 -0800

    adding notes and skeleton

---
+++ assets/employee.js
@@ -0,0 +1,10 @@
+
+class Employee{
+    constructor(id,name,email){
+        this.id=id
+        this.name=name
+        this.email=email
+    }
+}
+
+module.exports=Employee
---
+++ assets/intern.js
@@ -0,0 +1,9 @@
+const Employee= require('./employee')
+
+class Intern extends Employee{
+
+    constructor(id,name,email,school){
+        super(id,name,email)
+        this.school=school
+    }
+}
