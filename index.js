4617871d70accc45ff39e6d8a1e7bad268292880
JonkHunkle <88416486+JonkHunkle@users.noreply.github.com>
2021-11-20 19:34:52 -0800

    questions done parsing info for html file write

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 const inquirer=require('inquirer')
+const fs=require('fs')
+const Intern=require('./lib/intern')
+const Engineer=require('./lib/engineer')
+var Manager=require('./lib/manager')
+const fakeHTML = require('./HTMLcreate')
+var team=[]
 
 
-
-function managerQ(){
+function init(){
     inquirer.prompt([
         {   
             type:'confirm',
-            name:'manager',
-            message:'Are you the manager?'
-        },
-        {
-            type:'input',
-            name:'officeNum',
-            message:'What is your office number?',
-        },
-        {
-            type:'confirm',
-            name:'addUser',
-            message:'Would you like to add anyone?',
+            name:'role',
+            message:'Are you the manager?',
             
         },
-       
-    ])
-    .then((answers)=>{
-        if (answers.addUser===true){
-        employeeQ()}
+    ]).then((answers)=>{
+        if (answers.manager===false){
+            console.log('go get the manager')
+        }else managerQ()
+        
+        
         
     })
 }
 
-function employeeQ(){
+function managerQ(){
+    inquirer.prompt([
+       
+        {
+            type:'input',
+            name:'office',
+            message:'What is your office number?',
+        }       
+    ])
+    .then((answer)=>{
+     employeeQ(answer)}
+     
+)}
+
+function employeeQ(data, answer){
     inquirer.prompt([
         {
             type:'input',
@@ -47,58 +57,91 @@ function employeeQ(){
             message:'What is your ID?'
         }, 
         {
-            message:'What is your position',
-            type:'list',
-            name:'position',
-            choices:['Engineer', 'Intern']
-        }
+            message:'Is there anyone else on the team?',
+            type:'confirm',
+            name:'add',
+            
+        } 
         
     ]).then((answers)=>{
-        if(answers.position==='Engineer'){
-            engineer()
-        }else intern()})
+        //add code to push new employee to team array
+       console.log('this is data.role',data.role)
+      
+       
+       if(data.office){
+        const newManager= new Manager(answers.username, answers.idNum, answers.email, data.office)
+        console.log('in new manager class', newManager)
+       team.push(newManager)
+       }else
+       if(data.role==='Engineer'){
+        
+        const newEngineer=new Engineer(answers.username, answers.idNum, answers.email, answer.github,data.role)
+            console.log('in new engineer class',newEngineer)   
+        team.push(newEngineer)
+       }else if(data.role==='Intern'){
+          
+        const newIntern= new Intern(answers.username, answers.idNum, answers.email, answer.school, data.role)
+        console.log('in new intern class',newIntern)
+        team.push(newIntern)
+        }
+        if(answers.add){
+            newEmployee()
+    }else {writeHTML()}
+console.log('this is the team in index',team)})
 }
 
+function newEmployee(){
+    inquirer.prompt([
+        {   
+            message:'what is your role?',
+            choices:['Engineer','Intern'],
+            type:'list',
+            name:'role'
+        }
+    ]).then((answer)=>{
+        if (answer.role==='Engineer'){
+            engineer(answer)
+        }else {intern(answer)}
+    })
+}
 
-function engineer(){
+function engineer(data){
     inquirer.prompt([
         {
             type:'input',
             name:'github',
             message:'What is your github?'
-        },
-        {
-            type:'confirm',
-            name:'addAnother',
-            message:'Would you like to add another employee?',
-            
-        }
-    ]).then((answers)=>{
-        if(answers.addAnother===true){employeeQ()}
-    })
+        } 
+        
+    ]).then((answer)=>{employeeQ(data, answer)})
 }
 
-function intern(){
+function intern(data){
     inquirer.prompt([
         {
             type:'input',
             name:'school',
             message:'Where did you attend school?'
-        },{
-            type:'confirm',
-            name:'addAnother',
-            message:'Would you like to add another employee?',
-            
         }
-    ]).then((answers)=>{
-        if(answers.addAnother===true){employeeQ()}
-    })
+    ]).then((answer)=>{employeeQ(data, answer)})
+         
 }
 
 
-managerQ()
 
+function writeHTML(){
+    console.log("write html", team)
+fs.writeFile('./dist/indexfun.html', fakeHTML(team),
+    (err) =>
+      err ? console.error(err) : console.log("Success!")
+  )}
 
+// function init() {
+//     inquirer.prompt(questionsManager) 
+//     .then(function answers(answers) {
+//         const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+//         employeesArray.push(newManager)
+//         console.log(employeesArray)
 
 
 
@@ -113,3 +156,4 @@ managerQ()
 //profit
 
 
+init()
